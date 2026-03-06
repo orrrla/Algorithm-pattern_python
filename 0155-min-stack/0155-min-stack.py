f3b677a41ad6b4610e4cd032174389f04ae55bce
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-06 15:20:55 +0800

    Time: 11 ms (27.66%), Space: 22.4 MB (19.75%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,24 +1,23 @@
 class MinStack:
-
     def __init__(self):
-        self.stack = []
+        self.stack = []      # 数据栈
+        self.min_stack = []  # 最小值栈
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        # 如果最小值栈为空，或者 val 小于等于当前最小值，则入最小值栈
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
 
     def pop(self) -> None:
-        self.stack.pop()
+        if self.stack:
+            val = self.stack.pop()
+            # 如果弹出的值等于当前最小值，也从最小值栈弹出
+            if self.min_stack and val == self.min_stack[-1]:
+                self.min_stack.pop()
 
     def top(self) -> int:
-        return self.stack[-1]
+        return self.stack[-1] if self.stack else None
 
     def getMin(self) -> int:
-        return min(self.stack)
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
+        return self.min_stack[-1] if self.min_stack else None
