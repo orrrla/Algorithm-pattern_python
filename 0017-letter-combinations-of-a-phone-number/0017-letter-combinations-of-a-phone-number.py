99bc3b37f5d0ca982876ec3e9288dcaa24e2f89b
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-21 16:05:21 +0800

    Time: 0 ms (100%), Space: 19 MB (70.85%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,18 @@
+mapping = ['','','abc','def','ghi','jkl','mno','pqrs','tuv','wxyz']
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        n = len(digits)
+        if n == 0:
+            return []
+        path = ['']*n
+        ans = []
+        def dfs(i):
+            if i == n:
+                ans.append(''.join(path))
+                return
+            for c in mapping[int(digits[i])]:
+                path[i] = c
+                dfs(i+1)
+            
+        dfs(0)
+        return ans
