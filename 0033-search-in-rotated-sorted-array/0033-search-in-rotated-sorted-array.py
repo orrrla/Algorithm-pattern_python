eab6e852a3bf1d6073a14d3aa32df340096dfb5e
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-23 23:39:06 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,23 +1,20 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
-        
+        n = len(nums)
+        left,right = 0, n - 1
         while left <= right:
-            mid = (left + right) // 2
-            
+            mid = left + (right - left) // 2
             if nums[mid] == target:
                 return mid
-            
-            # 判断左半部分是否有序
-            if nums[left] <= nums[mid]:  # 左半部分有序
+
+            if nums[left] < nums[mid]:
                 if nums[left] <= target < nums[mid]:
-                    right = mid - 1  # target在左半部分
+                    right = mid - 1
                 else:
-                    left = mid + 1   # target在右半部分
-            else:  # 右半部分有序
+                    left = mid + 1
+            else:
                 if nums[mid] < target <= nums[right]:
-                    left = mid + 1   # target在右半部分
+                    left = mid + 1
                 else:
-                    right = mid - 1  # target在左半部分
-        
+                    right = mid - 1
         return -1
