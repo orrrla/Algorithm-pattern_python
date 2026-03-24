a73312a2c978206404e190428a1c563070d28854
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-24 17:51:18 +0800

    Time: 183 ms (45.2%), Space: 42.8 MB (6.57%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,18 +5,16 @@ class MedianFinder:
         self.left = []
         self.right = []
     def addNum(self, num: int) -> None:
-        # heapq.heappush(self.left, -num)  # 存负数
+        if len(self.left) == len(self.right):
+            heapq.heappush(self.left, - heapq.heappushpop(self.right, num))
+        else:
+            heapq.heappush(self.right,-heapq.heappushpop(self.left, -num))
         
-        # 移动左堆最大到右堆
-        heapq.heappush(self.right, num)
-
-        if len(self.left) < len(self.right):
-            heapq.heappush(self.left, -heapq.heappop(self.right))  # 存负数
     
     def findMedian(self) -> float:
         if len(self.left) > len(self.right):
             return -self.left[0]  # 取负得到原值
-        return (-self.left[0] + self.right[0]) / 2.0
+        return (-self.left[0] + self.right[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
