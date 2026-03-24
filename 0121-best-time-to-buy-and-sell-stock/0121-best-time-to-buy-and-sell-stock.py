28ff2c3befb7e74f72210c723f487327d2d8937a
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-24 18:00:55 +0800

    Time: 55 ms (66.58%), Space: 28 MB (79.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,22 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:          
-        def helper(left, right):
-            # 递归终止条件
-            if left >= right:
-                return 0
-            # 分割数组
-            mid = (left + right) // 2
-            # 左右子数组的最大利润
-            left_profit = helper(left, mid)
-            right_profit = helper(mid + 1, right)
-
-            # 跨边界的最大利润
-            left_min = min(prices[left:mid + 1])
-            right_max = max(prices[mid + 1:right + 1])
-            cross_profit = right_max - left_min
-            return max(left_profit, right_profit, cross_profit)
-        if not prices or len(prices) < 2:
-            return 0
-
-        return helper(0, len(prices) - 1)
+        min_price = prices[0]
+        ans = 0
+        for p in prices:
+            ans = max(ans, p-min_price)
+            min_price = min(p,min_price)
+        return ans
 
