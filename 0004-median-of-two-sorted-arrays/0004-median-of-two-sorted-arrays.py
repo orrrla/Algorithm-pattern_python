d372cbf494a70d41e06a538b8276223b86a37761
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-24 13:30:59 +0800

    Time: 3 ms (59.06%), Space: 19.5 MB (13.55%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,24 +1,18 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        # 合并两个数组
-        merged = []
-        i = j = 0
-        
-        while i < len(nums1) and j < len(nums2):
-            if nums1[i] <= nums2[j]:
-                merged.append(nums1[i])
-                i += 1
-            else:
-                merged.append(nums2[j])
-                j += 1
-        
-        # 添加剩余元素
-        merged.extend(nums1[i:])
-        merged.extend(nums2[j:])
-        
-        # 找中位数
-        n = len(merged)
-        if n % 2 == 1:
-            return merged[n // 2]
-        else:
-            return (merged[n//2 - 1] + merged[n//2]) / 2
+    def findMedianSortedArrays(self, a: List[int], b: List[int]) -> float:
+        if len(a) > len(b):
+            a,b = b,a 
+        m,n = len(a),len(b)
+        a = [-inf] + a + [inf]
+        b = [-inf] + b + [inf]
+
+        i = 0
+        j = (m+n+1)//2-i
+
+        while True:
+            if a[i]<=b[j+1] and b[j]<a[i+1]:
+                num1 = max(a[i],b[j])
+                num2 = min(a[i+1],b[j+1])
+                return (num1+num2)/2 if (m+n)%2 == 0 else num1
+            i+=1
+            j-=1
