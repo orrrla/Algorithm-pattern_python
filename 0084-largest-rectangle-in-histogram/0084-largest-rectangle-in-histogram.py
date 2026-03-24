eeb67df42db91d2dcff1ece41befa8b3ef78ebde
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-24 15:26:33 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -7,20 +7,11 @@ class Solution:
 
         for i,h in enumerate(heights):
             while st and heights[st[-1]]>=h:
-                st.pop()
+                rigth[st.pop()]=i
             if st:
                 left[i] = st[-1]
             st.append(i)
 
-        st.clear()
-
-        for i in range(n-1,-1,-1):
-            h = heights[i]
-            while st and heights[st[-1]]>=h:
-                st.pop()
-            if st:
-                right[i] = st[-1]
-            st.append(i)
         ans=0
         for h,l,r in zip(heights,left,right):
             ans = max(ans,h*(r-l-1))
