5574afafd369653150e85b710e84371816d23979
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-24 17:44:45 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -5,10 +5,10 @@ class MedianFinder:
         self.left = []
         self.right = []
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.left, -num)  # 存负数
+        # heapq.heappush(self.left, -num)  # 存负数
         
         # 移动左堆最大到右堆
-        heapq.heappush(self.right, -heapq.heappop(self.left))
+        heapq.heappush(self.right, num)
 
         if len(self.left) < len(self.right):
             heapq.heappush(self.left, -heapq.heappop(self.right))  # 存负数
