608a749cbcf202fb9078aa4ea5be043c2768c56c
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-28 14:22:57 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def dfs(i):
+            if i == 1:
+                return 1
+            if i == 2:
+                return 2
+            return dfs(i-1)+dfs(i-2)
+        return dfs(n)
