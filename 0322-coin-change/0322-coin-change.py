cfa6d45b6c2656c55b4573ee2333838f2a76d658
Xu minxiu <86822665+orrrla@users.noreply.github.com>
2026-03-28 14:50:10 +0800

    Time: 765 ms (34.95%), Space: 21 MB (27.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,6 +2,7 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
         f = [[inf]*(amount+1) for _ in range(n+1)]
+        f[0][0] = 0
         for i, x in enumerate(coins):
             for c in range(amount+1):
                 if c < x:
